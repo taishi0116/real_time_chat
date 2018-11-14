fe8c5f5fc750256b51dfbd3706e15527bbea773c
taishi0116 <bobochin16@gmail.com>
2018-11-14 11:51:02 +0000

    second commit

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -6,8 +6,8 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
-
+  
   def speak(data)
-    ActionCable.server.broadcast 'room_channel', message: data['message']
+    Message.create! content: data['message']
   end
 end
